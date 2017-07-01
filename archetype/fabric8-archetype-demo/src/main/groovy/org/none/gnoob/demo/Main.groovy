1f5a5609c9eef0bc3d7240665ca02fe6d74b0ca7
G Pelosi <hute37@gmail.com>
2017-07-01 19:13:08 +0200

    fabric8 funktion archetype

---
+++ archetype/fabric8-archetype-demo/src/main/groovy/org/none/gnoob/demo/Main.groovy
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2016 Red Hat, Inc.
+ * <p>
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+package org.none.gnoob.demo
+
+import org.apache.camel.Header
+
+def main(String body, @Header("name") String name) {
+    if (name != null) {
+      "Hello ${name}! I got payload `${body}` and I am on host: ${System.getenv("HOSTNAME")} and I am groovy!"
+    } else {
+      "What is your name? Specify a name using ?name=foo as query parameter. I am on host: ${System.getenv("HOSTNAME")} and I am groovy!"
+    }
+}
